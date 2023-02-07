0e2cf076239d7ad4ecde1473a499c15a524193d6
Ryan <wrosullivan88@gmail.com>
2023-02-07 14:36:15 -0600

    reactstrap and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+
+
+    private
+    
+    def user_params #come back!
+        params.permit(:first_name, :last_name, :password)
+    end
+
+
+
+    #
 end
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, Row, Col, Label, Input, FormGroup } from 'reactstrap';
 
 
 
@@ -7,21 +8,67 @@ import React from "react";
 
 
 
-function Login () {
+function Example(args) {
+    const [modal, setModal] = useState(false);
 
-
-
-
-
-
-
-
+    const toggle = () => setModal(!modal);
 
     return (
-        <div>
-            Login test
-        </div>
-    );
-};
+    <div>
+      <Button color="success" onClick={toggle}>
+        Login
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} {...args}>
+        <ModalHeader toggle={toggle}>Please enter your email and password below.</ModalHeader>
+        <ModalBody>
+        <Form>
+  <Row className="row-cols-lg-auto g-3 align-items-center">
+    <Col>
+      <Label
+        className="visually-hidden"
+        for="exampleEmail"
+      >
+        Email
+      </Label>
+      <Input
+        id="exampleEmail"
+        name="email"
+        placeholder="email address"
+        type="email"
+      />
+    </Col>
+    <Col>
+      <Label
+        className="visually-hidden"
+        for="examplePassword"
+      >
+        Password
+      </Label>
+      <Input
+        id="examplePassword"
+        name="password"
+        placeholder="password"
+        type="password"
+      />
+    </Col>
+    <Col>
 
-export default Login
+    </Col>
+
+  </Row>
+</Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="success" onClick={toggle}>
+            Login
+          </Button>{' '}
+          <Button color="warning" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default Example;
